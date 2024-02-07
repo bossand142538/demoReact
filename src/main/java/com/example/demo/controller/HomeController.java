99162ef265db9d4593a4648f9f5d8a2c86c5404e
Harit Chawalitbenja <harit.c@arise.tech>
2024-02-07 11:30:27 +0700

    test

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping
-    public String getHome() {
+    public String getHome(Model model) {
+        model.addAttribute("message", "Hello World");
         return "index";
+    }
+
+    @GetMapping("style")
+    public String style(Model model) {
+        model.addAttribute("message", "Hello World");
+        return "add-css-js-demo";
     }
 }
